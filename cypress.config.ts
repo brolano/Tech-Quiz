3d37531cea47cff1d82985b320b95a06751d3bec
Brian Solano <brian_solano110012@yahoo.com>
2025-02-24 20:47:31 -0600

    revised config for cypress to actually run tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from "cypress";
+// cypress.config.ts
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3002', // adjust this to match your development server
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  component: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: 'cypress/support/component.ts',
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    devServer: {
+      framework: 'react',
+      bundler: 'vite', // or 'webpack' depending on your setup
     },
   },
 });
